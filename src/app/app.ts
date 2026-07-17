be8e460637a30c2b6a57d72d73ca599aaac9ce21
Parimal Matte <123257338+PmCoder26@users.noreply.github.com>
2026-07-17 12:37:03 +0530

    feat(angular): implement two-way data binding with user input fields

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -13,4 +14,22 @@ export class App {
 
   // users: string[] = ['Parimal', 'Rohit', 'Hemant', 'Karunya', 'Shashank', 'Ishant']
   users: string[] = []  // for 'empty() {}' block.
+
+  // Two Way Binding.
+  userName: string = "";
+  age: number = 0;
+  isAdmin: boolean = false;
+  
+  loadSampleData() {
+    this.userName = 'Tony';
+    this.age = 42;
+    this.isAdmin = true;
+  }
+
+  resetSampleData() {
+    this.userName = '';
+    this.age = 0;
+    this.isAdmin = false;
+  }
+
 }
