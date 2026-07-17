ece70ab9fc7f99e9c23ce11402b43cd52b1e6907
Parimal Matte <123257338+PmCoder26@users.noreply.github.com>
2026-07-17 20:07:01 +0530

    feat(todos): implement todo list and learn two-way binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { Todo } from './models/todos';
 
 @Component({
   selector: 'app-root',
@@ -30,6 +31,38 @@ export class App {
     this.userName = '';
     this.age = 0;
     this.isAdmin = false;
+  }
+
+  // To-Dos List.
+  todos: Todo[] = [];
+  taskString: string = '';
+  warningMessge: string = '';
+
+  addTask() {
+    if(this.taskString == '') {
+      this.warningMessge = 'Task cannot be empty!';
+      return;
+    }
+
+    const id = this.todos.length === 0 ? 0 : this.todos[this.todos.length - 1].id + 1;
+
+    const todo: Todo = {
+      id: id,
+      title: this.taskString,
+      completed: false
+    };
+    this.todos.push(todo);
+
+    this.taskString = '';
+    this.warningMessge = '';
+  }
+
+  deleteTask(id: number) {
+    this.todos = this.todos.filter((todo) => todo.id != id);
+  }  
+
+  clearWarning() {
+    this.warningMessge = '';
   }
 
 }
